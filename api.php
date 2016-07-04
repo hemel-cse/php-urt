717de67f44fa4ff065362ae3b531f2f5b1cd57fc
Nadim Tuhin <nadimtuhin@gmail.com>
2016-07-05 03:42:41 +0600

    added api.php

---
+++ api.php
@@ -0,0 +1,16 @@
+<?php 
+require ('vendor/autoload.php');
+require ('functions.php');
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+$host = '45.119.120.2';
+// $host = '91.121.183.25';
+
+$host = '45.119.120.2';
+$port = 27960;
+
+$data = getUrtServerStatus($host, $port, 15);
+$response = new JsonResponse();
+$response->setData($data);
+@$response->send();
